00eddf61b8fb01420cf3313711b4a219761b81f4
devbrendacosta-oss <devbrendacosta@gmail.com>
2026-09-03 16:01:12 -0300

    Produto.java

--- Produto.java
+++ Produto.java
@@ -1,11 +1,6 @@
 package br.edu.aula;
 
 /**
- * Classe Produto
- *
- * Mantida simples, do mesmo jeito que já vinha sendo usada nas aulas:
- * apenas atributos públicos, sem métodos.
- *
  * A única mudança foi acrescentar o atributo "cadastrado", do tipo boolean,
  * exatamente como sugerido no próprio enunciado da Etapa 6, para o sistema
  * conseguir saber se já existe ou não um produto cadastrado.
